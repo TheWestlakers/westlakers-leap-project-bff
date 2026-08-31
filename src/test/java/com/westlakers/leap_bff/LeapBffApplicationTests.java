00fe353f414b8e4e50f31f65181ce0347faee300
krishalachhetri <krishalachhetri2022@gmail.com>
2026-08-31 21:29:06 +0000

    initial Spring Boot BFF setup

---
+++ src/test/java/com/westlakers/leap_bff/LeapBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westlakers.leap_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
