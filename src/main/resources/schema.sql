1fc84b977c83ba257f42d8c69a2e0e48b6fbfb1b
AlexVu2662 <avu9345@gmail.com>
2026-09-16 15:25:29 +0000

    removed currency column from account table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -75,7 +75,6 @@ CREATE TABLE IF NOT EXISTS accounts (
                           account_type_id INTEGER NOT NULL,
                           account_status_id INTEGER NOT NULL,
                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          currency VARCHAR(10) NOT NULL,
                           settled_cash NUMERIC(19,4) DEFAULT 0.00,
 
                           CONSTRAINT fk_account_user
